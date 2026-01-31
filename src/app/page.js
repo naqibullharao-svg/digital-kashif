e69107e29bdf71b0a203e8f595a597d759b422d7
naqibullharao-svg <naqibullharao@gmail.com>
2026-01-31 13:12:55 +0500

    Create page.js
    home page added

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+Export default function Home() {
+  return (
+    <main style={{ padding: "30px", fontFamily: "Arial" }}>
+      <h1>Rao Kashif Digital Services</h1>
+      <p>Guest Posting, Link Insertions & SEO Backlinks</p>
+
+      <a href="/order">Order Now</a>
+    </main>
+  );
+}
